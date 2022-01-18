141f5606e5a3c543952b7b5a0f4c893e9d6b1a9d
an_owl <an_owl@protonmail.com>
2022-01-18 16:50:39 +1300

    added prototypes to lib

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub struct InternalState{
+    pub time: usize,
+    pub no_stdout: bool,
+    pub suppress_notifications: bool,
+}
+
+pub fn update_time(is: &mut InternalState){
+    todo!()
+}
+
+pub fn timer_core(is: InternalState){
+    todo!()
+}
